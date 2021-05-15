6b13cdc86140beceeba5038e7bc2abf724bf9060
Ronaldo Jacobo <ronaldo.jacobo.26@gmail.com>
2021-05-15 14:59:07 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//Puedes poner el codigo html directamente con 'template'
+//template: '<h1>Hola de nuevo</h1>',
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
+
 export class AppComponent {
-  title = 'bases';
+  
 }
+
